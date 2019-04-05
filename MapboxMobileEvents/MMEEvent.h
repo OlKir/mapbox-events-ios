278dd63502591d4ace0e1388a32e9c8179987192
Alf Watt <alf.watt@mapbox.com>
2019-04-04 22:30:05 -0700

    Group the deprecated event constructors, document the rest

--- MapboxMobileEvents/MMEEvent.h
+++ MapboxMobileEvents/MMEEvent.h
@@ -1,12 +1,13 @@
 #import <Foundation/Foundation.h>
 
-@class MMEDate;
+@class MMEDate; // MAKE PRIVATE
 @class MMECommonEventData;
 
 /*! @brief represents a telemetry event, with date, name and attributes */
 @interface MMEEvent : NSObject <NSCopying,NSSecureCoding>
 
-/*! @brief date on which the event occured, including the local time offset */
+/*! @brief date on which the event occured, including the local time offset
+    @note MAKE READONLY */
 @property (nonatomic, copy) MMEDate *date;
 
 /*! @brief name of the event */
@@ -17,26 +18,96 @@
 
 #pragma mark -
 
+/*! @brief eventWithDate:name:attributes:
+    @param eventDate date
+    @param name name
+    @param attributes attrs
+    @return event
+    @note MAKE PRIVATE
+*/
 + (instancetype)eventWithDate:(MMEDate *)eventDate name:(NSString *)name attributes:(NSDictionary *)attributes;
+
+/*! @brief eventWithName:attributes:
+    @param eventName name
+    @param attributes attrs
+    @return event
+*/
 + (instancetype)eventWithName:(NSString *)eventName attributes:(NSDictionary *)attributes;
 
+/*! @brief turnstileEventWithAttributes:
+    @param attributes attrs
+    @return event
+*/
 + (instancetype)turnstileEventWithAttributes:(NSDictionary *)attributes;
-+ (instancetype)telemetryMetricsEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
+
+/*! @brief locationEventWithAttributes:instanceIdentifer:commonEventData:
+    @param attributes attrs
+    @param instanceIdentifer identifier
+    @param commonEventData data
+    @return event
+*/
 + (instancetype)locationEventWithAttributes:(NSDictionary *)attributes instanceIdentifer:(NSString *)instanceIdentifer commonEventData:(MMECommonEventData *)commonEventData;
+
+/*! @brief visitEventWithAttributes:
+    @param attributes attrs
+    @return event
+*/
 + (instancetype)visitEventWithAttributes:(NSDictionary *)attributes;
+
+/*! @brief navigationEventWithName:attributes:
+    @param name name
+    @param attributes attrs
+    @return event
+*/
++ (instancetype)navigationEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
+
+/*! @brief visionEventWithName:attributes:
+    @param name name
+    @param attributes attrs
+    @return event
+*/
++ (instancetype)visionEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
+
+/*! @brief debugEventWithAttributes:
+    @param attributes attrs
+    @return event
+*/
++ (instancetype)debugEventWithAttributes:(NSDictionary *)attributes;
+
+/*! @brief debugEventWithError:
+    @param error error
+    @return event
+*/
++ (instancetype)debugEventWithError:(NSError*) error;
+
+/*! @brief debugEventWithException:
+    @param except exception
+    @return event
+*/
++ (instancetype)debugEventWithException:(NSException*) except;
+
+/*! @brief searchEventWithName:attributes:
+    @param name name
+    @param attributes attrs
+    @return event
+*/
++ (instancetype)searchEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
+
+/*! @brief carplayEventWithName:attributes:
+    @param name name
+    @param attributes attrs
+    @return event
+*/
++ (instancetype)carplayEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
+
+#pragma mark - Depricated (Date Strings)
+
++ (instancetype)telemetryMetricsEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
 + (instancetype)mapLoadEventWithDateString:(NSString *)dateString commonEventData:(MMECommonEventData *)commonEventData;
 + (instancetype)mapTapEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
 + (instancetype)mapDragEndEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
 + (instancetype)mapOfflineDownloadStartEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
 + (instancetype)mapOfflineDownloadEndEventWithDateString:(NSString *)dateString attributes:(NSDictionary *)attributes;
-+ (instancetype)navigationEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
-+ (instancetype)visionEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
-+ (instancetype)debugEventWithAttributes:(NSDictionary *)attributes;
-+ (instancetype)debugEventWithError:(NSError*) error;
-+ (instancetype)debugEventWithException:(NSException*) except;
-+ (instancetype)searchEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
-+ (instancetype)carplayEventWithName:(NSString *)name attributes:(NSDictionary *)attributes;
 + (instancetype)eventWithDateString:(NSString *)dateString name:(NSString *)name attributes:(NSDictionary *)attributes;
-
 
 @end
