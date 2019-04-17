9f0552460d27d31e6b364789a129d8460864a225
Alf Watt <alf.watt@mapbox.com>
2019-04-17 15:46:05 -0700

    Maybe oneway we'll use these for something

--- MapboxMobileEventsTests/MMEDateFakes.h
+++ MapboxMobileEventsTests/MMEDateFakes.h
@@ -2,13 +2,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-// some fake dates for testing
+/*! @brief some fake dates with sloppy matching for testing MMEEvents */
 @interface MMEDateFakes : MMEDate
 
-+ (MMEDate *)earlier; // an MMEDate which isEqual: and can compare: as equal to any date which is earlier on the timeline than it's creation
-+ (MMEDate *)later; // an MMEDate which isEqual: to and can compare: as equal to any date which is later on the timeline than it's creation
-+ (MMEDate *)whenever; // an MMEDate which isEqual: to all NSDates and will compare: as equal to all dates
-+ (MMEDate *)never; // an MMEDate which never isEqual: to any date and will never compare: as equal to any date
+/*! @brief an MMEDate which isEqual: and can compare: as equal to any date which is earlier on the timeline than it's creation */
++ (MMEDate *)earlier;
+
+/*! @brief an MMEDate which isEqual: to and can compare: as equal to any date which is later on the timeline than it's creation */
++ (MMEDate *)later;
+
+/*! @brief an MMEDate which isEqual: to all NSDates and will compare: as equal to any date */
++ (MMEDate *)whenever;
+
+/*! @brief an MMEDate which never isEqual: to any date and will never compare: as equal to any date */
++ (MMEDate *)never;
 
 @end
 
